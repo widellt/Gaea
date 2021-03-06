78e02214fd1cb332990fb8835a2c08be7b17d58b
widellt <tywidell@gmail.com>
2021-03-05 19:58:49 -0800

    Added Premake
    Added premake build configurator

---
+++ premake5.lua
@@ -0,0 +1,96 @@
+workspace "Gaea"
+	architecture "x64"
+
+	configurations{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}" 
+
+project "Gaea"
+	location "Gaea"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs {
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+			"GA_PLATFORM_WINDOWS",
+			"GA_BUILD_DLL"
+		}
+		
+		postbuildcommands {
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug" 
+		defines "GA_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release" 
+		defines "GA_RELEASE"
+		optimize "On"
+	
+	filter "configurations:Dist" 
+		defines "GA_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs {
+		"Gaea/vendor/spdlog/include",
+		"Gaea/src"
+	}
+
+	links {
+		"Gaea"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+			"GA_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug" 
+		defines "GA_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release" 
+		defines "GA_RELEASE"
+		optimize "On"
+	
+	filter "configurations:Dist" 
+		defines "GA_DIST"
+		optimize "On"
