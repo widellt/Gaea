9034da37ee4bcb77923040899cf7f105b59551c3
widellt <tywidell@gmail.com>
2021-03-16 10:01:53 -0700

    Added buffer layouts: Can now set and get vertex buffer layouts within application

--- Gaea/src/Gaea/Application.cpp
+++ Gaea/src/Gaea/Application.cpp
@@ -13,6 +13,26 @@ namespace Gaea {
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type) {
+		switch (type)
+		{
+			case ShaderDataType::Float:		return GL_FLOAT;
+			case ShaderDataType::Float2:	return GL_FLOAT;
+			case ShaderDataType::Float3:	return GL_FLOAT;
+			case ShaderDataType::Float4:	return GL_FLOAT;
+			case ShaderDataType::Mat3:		return GL_FLOAT;
+			case ShaderDataType::Mat4:		return GL_FLOAT;
+			case ShaderDataType::Int:		return GL_INT;
+			case ShaderDataType::Int2:		return GL_INT;
+			case ShaderDataType::Int3:		return GL_INT;
+			case ShaderDataType::Int4:		return GL_INT;
+			case ShaderDataType::Bool:		return GL_BOOL;
+		}
+
+		GA_CORE_ASSERT(false, "Uknown ShaderDataType!");
+		return 0;
+	}
+
 	Application::Application() {
 		GA_CORE_ASSERT(!s_Instance, "Application already exists!");
 		s_Instance = this;
@@ -26,16 +46,33 @@ namespace Gaea {
 		glGenVertexArrays(1, &_VertexArray);
 		glBindVertexArray(_VertexArray);
 
-		float vertices[3 * 3] = { 
-			-0.5f, -0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f, 
-			0.0f, 0.5f, 0.0f, 
+		float vertices[3 * 7] = { 
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 0.8f,
+			0.0f, 0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 0.8f
 		};
 
 		_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);// Describing data at index 0, 3 floats
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
+
+			_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+
+		const auto& layout = _VertexBuffer->GetLayout();
+
+		for (const auto& element : layout) {
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, element.GetComponentCount(), ShaderDataTypeToOpenGLBaseType(element.Type), 
+				element.Normalized ? GL_TRUE : GL_FALSE, layout.GetStride(), (const void*)element.Offset);
+			index++;
+		}
 
 		uint32_t indices[3] = { 0, 1, 2 };
 		_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
@@ -44,12 +81,15 @@ namespace Gaea {
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 
 			out vec3 v_Position; 
+			out vec4 v_Color;
 
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);
 			}
 
@@ -59,12 +99,15 @@ namespace Gaea {
 			#version 330 core
 
 			layout(location = 0) out vec4 color;
+			
 
 			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 
 		)";
--- Gaea/src/Platform/OpenGL/OpenGLBuffer.h
+++ Gaea/src/Platform/OpenGL/OpenGLBuffer.h
@@ -11,10 +11,15 @@ namespace Gaea {
 		OpenGLVertexBuffer(float* vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
+
+		virtual const BufferLayout& GetLayout() const override { return _Layout; };
+		virtual void SetLayout(BufferLayout& layout) override { _Layout = layout; };
 	private:
 		uint32_t _RendererID;
+		BufferLayout _Layout;
+
 	};
 
 	///////////////////////////////////////////////////////
@@ -25,8 +30,8 @@ namespace Gaea {
 		OpenGLIndexBuffer(uint32_t* indices, uint32_t count);
 		virtual ~OpenGLIndexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
 
 		virtual uint32_t GetCount() const { return _Count; }
 	private:
